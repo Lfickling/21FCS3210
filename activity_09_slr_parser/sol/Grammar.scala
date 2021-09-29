804d77ff8382ae645ef81fd46b6572ce7f9d297c
Thyago Mota <thyagomota@gmail.com>
2021-09-28 20:40:46 -0600

    updates

---
+++ activity_09_slr_parser/sol/Grammar.scala
@@ -0,0 +1,43 @@
+/*
+ * CS3210 - Principles of Programming Languages - Fall 2021
+ * Instructor: Thyago Mota
+ * Description: Activity 09 - Grammar
+ */
+
+import SyntaxAnalyzer.GRAMMAR_FILENAME
+
+import scala.collection.mutable.ArrayBuffer
+import scala.io.Source
+
+class Grammar(private var source: String) {
+
+  private var productions = new ArrayBuffer[String]
+  for (line <- Source.fromFile(source).getLines)
+    productions += line
+
+  def getLHS(index: Integer): String = {
+    val production = productions(index)
+    production.split("->")(0).strip()
+  }
+
+  def getRHS(index: Integer) = {
+    val production = productions(index)
+    production.split("->")(1).strip().split(" ")
+  }
+
+  override def toString: String = {
+    var out = ""
+    for (i <- 0 until productions.length)
+      out += i + ". " + getLHS(i) + " -> " + getRHS(i).mkString(" ") + "\n"
+    out
+  }
+}
+
+object Grammar {
+
+  def main(args: Array[String]): Unit = {
+    val grammar = new Grammar("grammar.txt")
+    print(grammar.getRHS(2).mkString(" "))
+
+  }
+}
