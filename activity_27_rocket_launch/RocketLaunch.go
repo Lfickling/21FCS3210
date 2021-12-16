da879945df259681bb4d5339f72f23957beee2df
Lfickling <lettiefickling@gmail.com>
2021-12-15 18:26:59 -0700

    my changes

--- activity_27_rocket_launch/RocketLaunch.go
+++ activity_27_rocket_launch/RocketLaunch.go
@@ -2,12 +2,16 @@
  * CS3210 - Principles of Programming Languages - Fall 2020
  * Instructor: Thyago Mota
  * Description: Activity 22 - Rocket Launch Simulation
+ * go run RocketLaunch.go
  */
 
 package main
 
 import (
+	"fmt"
+	"math/rand"
 	"sync"
+	"time"
 )
 
 type Control struct {
@@ -31,6 +35,37 @@ var wg sync.WaitGroup
 // the operator should "sleep" for random seconds (<=10s)
 // add appropriate messages at each iteration
 func operator(name string, control *Control) {
+	defer wg.Done()
+	for {
+		control.mu.Lock()
+		if control.state == LAUNCHED {
+			control.mu.Unlock()
+			fmt.Printf("Operator %s is supes happy \n", name)
+			break
+		} else if control.state == ABORTED {
+			control.mu.Unlock()
+			fmt.Printf("Operator %s is frustrated with another aborted mission \n", name)
+			break
+		} else if control.state == SCHEDULED {
+			control.mu.Unlock()
+			fmt.Printf("Operator %s is taking a nap waiting for launch to be initiated \n", name)
+			time.Sleep(time.Duration(rand.Intn(5)) * time.Second)
+			fmt.Printf("Operator %s woke up!", name)
+		} else {
+			if rand.Intn(100) <= ABORT_PROBABILITY {
+				fmt.Printf("Operator %s is trying to abort the launch \n", name)
+				control.state = ABORTED
+				fmt.Printf("Rocket launch aborted by Operator %s \n", name)
+				control.mu.Unlock()
+				break
+			} else {
+				control.mu.Unlock()
+				fmt.Printf("Operator %s is taking a nap waiting for launch \n", name)
+				time.Sleep(time.Duration(rand.Intn(10)) * time.Second)
+				fmt.Printf("Operator %s woke up! \n", name)
+			}
+		}
+	}
 
 }
 
@@ -42,7 +77,34 @@ func operator(name string, control *Control) {
 // and change the state to LAUNCHED;
 // if the state is ABORTED, display a message (saying that the launch was abborted by the operator) and finish execution
 func launch(sec int, control *Control) {
-
+	defer wg.Done()
+	for {
+		control.mu.Lock()
+		if control.state == SCHEDULED {
+			control.state = INITIATED
+			control.mu.Unlock()
+		} else if control.state == INITIATED {
+			control.mu.Unlock()
+			fmt.Println("seconds until launch: ", sec)
+			if sec == 0 {
+				control.mu.Lock()
+				control.state = LAUNCHED
+				control.mu.Unlock()
+				println("LIFT OFF!!! rocket has been launched")
+				break
+			} else if sec <= 15 {
+				time.Sleep(time.Duration(rand.Intn(1)) * time.Second)
+				sec -= 1
+			} else if sec > 15 {
+				time.Sleep(time.Duration(rand.Intn(5)) * time.Second)
+				sec -= 5
+			}
+		} else if control.state == ABORTED {
+			control.mu.Unlock()
+			println("ABORT! ABORT! Launch has been aborted by the operator")
+			break
+		}
+	}
 }
 
 // TODO: run simulations changing ABORT_PROBABILITY and number of operators
@@ -51,11 +113,14 @@ func main() {
 	control.state = SCHEDULED
 
 	// TODO: change it based on #operators + 1 (the rocket)
-	// wg.Add(?)
+	wg.Add(3)
 
 	// TODO: launch a rocket!
+	go launch(20, &control)
 
 	// TODO: run operator goroutines
+	go operator("jane", &control)
+	go operator("Lettie", &control)
 
 	wg.Wait()
 }
